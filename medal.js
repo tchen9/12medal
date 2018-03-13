29c95d949444eb3e825ad0b10d6c9b4575daa15c
Tina <tchen9@stuy.edu>
2018-03-13 15:44:31 -0400

    started

---
+++ medal.js
@@ -0,0 +1,14 @@
+//US 9 8 6
+//REPUBLIC OF KOREA 5 8 4
+
+
+var svg = document.getElementById("s");
+
+var circle = function(){
+}
+
+var erase = function(){
+    svg.innerHTML = "";
+}
+
+clear.addEventListener("click", erase);
