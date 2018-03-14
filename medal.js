a421ed1207a7dcda49589d3d79d27ec769736089
angelng1412 <ang7@stuy.edu>
2018-03-13 23:16:16 -0400

    done

--- medal.js
+++ medal.js
@@ -1,14 +1,37 @@
 //US 9 8 6
 //REPUBLIC OF KOREA 5 8 4
-
-
 var svg = document.getElementById("s");
+var title = document.getElementById("country"); 
+var switchbtn = document.getElementById("switch");
+var country = 1; //1 for US of A, -1 for Korea
+var usa = [9, 8, 6];
+var korea = [5, 8, 4];
 
-var circle = function(){
+
+var changeTitle = function(){
+    if (country == 1){
+	title.innerHTML = "United States"; 
+    }
+    if (country == -1){
+	title.innerHTML = "Republic of Korea";
+    }
 }
 
-var erase = function(){
-    svg.innerHTML = "";
+var changeCircles = function(){
+    var cs = d3.selectAll("circle");
+    changeTitle(); 
+    if (country == 1){
+	cs.data(usa);
+	console.log(usa); 
+    }
+    if (country == -1){
+	cs.data(korea);
+	console.log(korea); 
+    }
+    cs.attr("r", function(d){ return d*5; });
+    country = country*-1
 }
 
-clear.addEventListener("click", erase);
+
+changeCircles(); 
+switchbtn.addEventListener("click", changeCircles); 
